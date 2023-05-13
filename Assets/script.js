4c0495f90c0f7f8d2b58bd24d4cbfa2abd60d090
annemarywynter1986 <missannemary1@gmail.com>
2023-05-12 21:09:18 -0400

    working on checkbox

--- Assets/script.js
+++ Assets/script.js
@@ -1,59 +1,64 @@
 // declare global variables
 
 // Set the beginning hour to be 8AM
-var hourBeginning = moment().startOf('day').add(7,'h');
+var hourBeginning = moment().startOf('day').add(7, 'h');
 // totalHours = 8AM to 6PM = 11 hours
-var totalHours = 11;
+var totalHours = 24;
 // Get current hour
 var currentHour = moment().format('H');
 // initializes timeTableElement and currentState of present, future, and past;
 var timeTableElement;
 var currentState;
+const checkboxes = Array.from(document.querySelectorAll('checkbox'));
 
-function chekboxFunction(){
-  var checkbox=$('#checkboxId')
-  var text=localStorage.keyValue;
-  {
-    if (checkbox.checked != true)
-    {
-      alert("you need to complete this task today");
+function chekboxFunction() {
+  var checkbox = $('#checkboxId')
+  var text = localStorage.keyValue;
+
+  checkboxes.forEach(checkboxes => {
+    const number = checkboxes.dataset['number']
+    checkbox.checked = currentHour['checkbox' + number]
+})
+
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', e => {
+        
+    if (checkbox.checked !== true) {
+     
+    $("textarea").css("text-decoration", "line-through");
+    alert("you completed your task")
+    
     }
-  }
-
-  // chekboxFunction($(this).checked)?$(this).attr('value',1):$(this).attr('value',0);
-  // if (checkbox.checked ===true){
-  //   text.style.display("text-decoration: line-through")
-  // } else{
-  //   text.style.display("text-decoration: none")
-  // }
-  // chekboxFunction();
+       
+    })
+})
+  chekboxFunction();
 }
 
-
 function displayToday() {
-    // today display
-    var today = moment().format("dddd, MMMM Do, HH:mm a");
-    $('#currentDay').text(today);
+  // today display
+  var today = moment().format("dddd, MMMM Do, HH:mm a");
+  $('#currentDay').text(today);
 }
 
 function fillTimeTable() {
 
-  for (var hour = 0; hour < totalHours; hour++) { 
-      var realHour = hour + 8;
-      // add one hour while iterating for loop
-      timeTableElement = hourBeginning.add(1,'h').format('HH:mm A');
+  for (var hour = 0; hour < totalHours; hour++) {
+    var realHour = hour + 8;
+    // add one hour while iterating for loop
+    timeTableElement = hourBeginning.add(1, 'h').format('HH:mm A');
 
-      // determine the currentState based on the conditions
-      if (currentHour == realHour) {
-          currentState = 'present';
-      } else if (currentHour < realHour) {
-          currentState = 'past';
-      } else {
-          currentState = 'future';
-      }
+    // determine the currentState based on the conditions
+    if (currentHour === realHour) {
+      currentState = 'present';
+    } else if (currentHour < realHour) {
+      currentState = 'past';
+    } else {
+      currentState = 'future';
+    }
 
-        var appendBlock = 
-            `<div id="hour-${realHour}" class="row time-block ${currentState}">
+    var appendBlock =
+      `<div id="hour-${realHour}" class="row time-block ${currentState}">
                 <div class="col-md-1 hour">${timeTableElement}</div>
                 <textarea class="col-md-8 description ${realHour}"></textarea>
                 <input type="checkbox" id="checkboxId" class="myCheck col-md-2" onclick="chekboxFunction()">
@@ -63,14 +68,14 @@ function fillTimeTable() {
                 
             </div>`;
 
-        $(".container").append(appendBlock);
-        
-       
-      
+    $(".container").append(appendBlock);
+
+
+
 
 
     loadSchedule();
-}
+  }
 
 }
 
@@ -79,24 +84,24 @@ function fillTimeTable() {
 // function for save schedule in the local storage
 function saveSchedule() {
 
-    var keyName = $(this).parent().attr('id');
-    var keyValue = $(this).parent().children().eq(1).val();
+  var keyName = $(this).parent().attr('id');
+  var keyValue = $(this).parent().children().eq(1).val();
 
-    localStorage.setItem(keyName, keyValue);
+  localStorage.setItem(keyName, keyValue);
 }
 
 // functino to get back the data from the local storage and print it out in to the textarea attribute
 function loadSchedule() {
 
-    for (var hour = 0; hour < totalHours; hour++) {
-        var realHour = hour + 8;
-        var loadedSchedule = localStorage.getItem(`hour-${realHour}`);
+  for (var hour = 0; hour < totalHours; hour++) {
+    var realHour = hour + 8;
+    var loadedSchedule = localStorage.getItem(`hour-${realHour}`);
 
-        $(`.${realHour}`).val(loadedSchedule);
-    }
-   
-    
-    }
+    $(`.${realHour}`).val(loadedSchedule);
+  }
+
+
+}
 
 
 
@@ -104,14 +109,15 @@ function loadSchedule() {
 displayToday();
 fillTimeTable();
 
+
 $('.saveBtn').on('click', saveSchedule);
 
 // updating date and time every 1 minute
-setInterval(function() {
-    displayToday();
+setInterval(function () {
+  displayToday();
 }, 60000);
 
 // updating entire time table every 1 hr
-setInterval(function() {
-    fillTimeTable();
+setInterval(function () {
+  fillTimeTable();
 }, 3.6e+6);
